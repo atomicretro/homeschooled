e380586e108a890adffe5939f1efe8ddeb3f16c2
alec cuccia <ajcuccia@gmail.com>
2019-06-01 20:03:52 -0400

    Create and style Home component; style Navbar; add placeholder img

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,15 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 
 import '../stylesheets/css/home.css';
 
-class Home extends Component {
-  render() {
-    return(
-      <section className="home">
-        <h1>Alec Cuccia</h1>
-      </section>
-    );
-  }
+const Home = (props) => {
+  const contentVisibility = props.navbarSize === 'large' ? 'invisible' : 'visible';
+  return (
+    <section className="home">
+      <div className={ `home__ego-section--${ props.navbarSize }` }>
+        <h1 className="home__ego">
+          Alec Cuccia
+        </h1>
+      </div>
+      <div className={ `home__cool-things--${ contentVisibility }` }>
+        <img src="marcy.png" alt="placeholder"/>
+      </div>
+    </section>
+  );
 }
 
-export default Home;
+const mapStateToProps = (state) => {
+  return { navbarSize: state.ui.navbarSize };
+};
+
+export default connect(mapStateToProps, null)(Home);
